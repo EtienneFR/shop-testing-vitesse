fcde5ea080b25f60c528ef9b9abffe29e3427949
Etienne Dayre <etiennedayre10@gmail.com>
2021-03-02 22:54:27 +0100

    feat: use fetch in netlify function

--- netlify/functions/search-api.js
+++ netlify/functions/search-api.js
@@ -1,6 +1,12 @@
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  }
-}
+const { URL } = require('url');
+const fetch = require('node-fetch');
+
+exports.handler = async() => {
+  const api = new URL('https://api.unsplash.com/search/photos?&orientation=landscape&query=products');
+
+  return fetch(api, { headers: { Authorization: process.env.TOKEN } })
+    .then(res => res.json())
+    .then((json) => {
+      console.log(json);
+    })
+};
