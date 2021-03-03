44d16fc69a9d2ad2bf79a95ec6290664c2df1508
Etienne Dayre <etiennedayre10@gmail.com>
2021-03-03 17:58:17 +0100

    feat: serverless function with promises

--- netlify/functions/search-api.js
+++ netlify/functions/search-api.js
@@ -1,12 +1,26 @@
 const { URL } = require('url');
 const fetch = require('node-fetch');
+const { promises } = require('fs');
 
 exports.handler = async() => {
   const api = new URL('https://api.unsplash.com/search/photos?&orientation=landscape&query=products');
 
-  return fetch(api, { headers: { Authorization: process.env.TOKEN } })
-    .then(res => res.json())
-    .then((json) => {
-      console.log(json);
+  const status = (response) => {
+    if (response.status >= 200 && response.status < 300)
+      return Promise.resolve(response)
+
+    return Promise.reject(new Error(response.statusText))
+  }
+
+  const json = response => response.json()
+
+  fetch(api, { headers: { Authorization: process.env.TOKEN } })
+    .then(status)
+    .then(json)
+    .then((data) => {
+      console.log('Request succeeded with JSON response', data)
+    })
+    .catch((error) => {
+      console.log('Request failed', error)
     })
 };
