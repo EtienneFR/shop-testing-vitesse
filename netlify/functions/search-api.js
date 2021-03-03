683058f42c965f42624296c4658210301482571e
Etienne Dayre <etiennedayre10@gmail.com>
2021-03-03 18:28:04 +0100

    fix: serverless function

--- netlify/functions/search-api.js
+++ netlify/functions/search-api.js
@@ -1,26 +1,14 @@
 const { URL } = require('url');
 const fetch = require('node-fetch');
-const { promises } = require('fs');
 
-exports.handler = async() => {
+exports.handler = async(event, context) => {
   const api = new URL('https://api.unsplash.com/search/photos?&orientation=landscape&query=products');
 
-  const status = (response) => {
-    if (response.status >= 200 && response.status < 300)
-      return Promise.resolve(response)
-
-    return Promise.reject(new Error(response.statusText))
-  }
-
-  const json = response => response.json()
-
-  fetch(api, { headers: { Authorization: process.env.TOKEN } })
-    .then(status)
-    .then(json)
-    .then((data) => {
-      console.log('Request succeeded with JSON response', data)
-    })
-    .catch((error) => {
-      console.log('Request failed', error)
-    })
+  return fetch(api, { headers: { Authorization: process.env.TOKEN } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data.response,
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
