f2200478a18b9a571a0f861cd7522598fe0b9f19
Etienne Dayre <etiennedayre10@gmail.com>
2021-12-10 10:53:47 +0100

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,7 @@ export default defineConfig({
 
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
+      registerType: 'autoUpdate',
       manifest: {
         name: 'Vitesse',
         short_name: 'Vitesse',
