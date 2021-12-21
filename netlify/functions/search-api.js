086aab6b004af1ca025d1b3ad341399938e48451
Etienne Dayre <etiennedayre10@gmail.com>
2021-12-21 11:36:20 +0100

    feat: use banned word api for string search

--- netlify/functions/search-api.js
+++ netlify/functions/search-api.js
@@ -4,19 +4,37 @@ const fetch = require('node-fetch')
 
 exports.handler = async(event, context) => {
   const query = event.queryStringParameters.query
-  const api = new URL(`https://api.unsplash.com/search/photos?&orientation=landscape&${new URLSearchParams({
+  const isAllowedApi = new URL(`https://neutrinoapi.net/bad-word-filter?${new URLSearchParams({
+    content: `${query}`,
+  })}`)
+
+  const searchApi = new URL(`https://api.unsplash.com/search/photos?&orientation=landscape&${new URLSearchParams({
     query: `${query}`,
   })}`)
 
-  return fetch(api, { headers: { Authorization: process.env.TOKEN } })
+  const isBad = await fetch(isAllowedApi, { headers: { 'api-key': process.env.APIKEY, 'user-id': process.env.USERID } })
     .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: JSON.stringify(data.results.map(product => ({
-        description: product.alt_description,
-        small: product.urls.small,
-        raw: product.urls.raw,
-      }))),
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+    .then(data => data['is-bad'])
+  const isDisallowedQuery = isBad === true
+  const isAllowedQuery = isDisallowedQuery === false
+
+  if (isAllowedQuery === true) {
+    return fetch(searchApi, { headers: { Authorization: process.env.TOKEN } })
+      .then(response => response.json())
+      .then(data => ({
+        statusCode: 200,
+        body: JSON.stringify(data.results.map(product => ({
+          description: product.alt_description,
+          small: product.urls.small,
+          raw: product.urls.raw,
+        }))),
+      }))
+      .catch(error => ({ statusCode: 422, body: String(error) }))
+  }
+  else {
+    return {
+      statusCode: 422,
+      body: 'error',
+    }
+  }
 }
